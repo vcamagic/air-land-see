2269c8f9b7189779eb2e04c36bbc47bac4bb6e9e
Aleksa Matic <a.matic@vegait.rs>
2022-05-19 16:27:35 +0200

    url change

--- client/src/websockets/WebSocketsProvider.tsx
+++ client/src/websockets/WebSocketsProvider.tsx
@@ -38,7 +38,7 @@ interface WebSocketProviderProps {
 export const WebSocketsProvider = ({ children }: WebSocketProviderProps) => {
   const connection = useRef(
     new HubConnectionBuilder()
-      .withUrl('http://localhost:5237/game')
+      .withUrl(''https://air-land-sea.herokuapp.com/game'')
       .configureLogging(LogLevel.Information)
       .build()
   );
@@ -54,7 +54,7 @@ export const WebSocketsProvider = ({ children }: WebSocketProviderProps) => {
 
   const joinGame = useCallback(async (name: string) => {
     connection.current = new HubConnectionBuilder()
-      .withUrl('http://localhost:5237/game')
+      .withUrl('https://air-land-sea.herokuapp.com/game')
       .configureLogging(LogLevel.Information)
       .build();
 
